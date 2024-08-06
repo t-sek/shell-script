e6bcf336a969ebf9131b9c6878e4c68451c4d1ed
t-sek <bobeechan@gmail.com>
2024-08-06 15:49:00 +0900

    Create measurement.sh

---
+++ measurement-loop/measurement.sh
@@ -0,0 +1,31 @@
+cat test.sh
+#!/bin/bash
+
+INPUT_FILE="your_input_file.txt"
+
+# 元のスクリプトの実行時間計測
+start=$(date +%s.%N)
+test="a"
+
+result=""
+
+while read line; do
+  # 行を変数に追加
+  result+="$line "
+done < $INPUT_FILE
+
+end=$(date +%s.%N)
+runtime=$(echo "$end - $start" | bc)
+
+echo "Runtime for while loop: $runtime seconds"
+
+# awkスクリプトの実行時間計測
+start=$(date +%s.%N)
+
+result=$(awk '{printf "%s ", $0}' $INPUT_FILE)
+
+
+end=$(date +%s.%N)
+runtime=$(echo "$end - $start" | bc)
+
+echo "Runtime for awk: $runtime seconds"
